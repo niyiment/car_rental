e0dc80e2ab127d7d427b681dc3c8718279e0b071
Niyi Ogungbemi <niyiment@gmail.com>
2025-11-09 22:46:08 +0100

    Create theme configuration and provider

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,22 @@
+ import 'package:flutter/material.dart';
+
+sealed class AppTheme {
+  static ThemeData get light => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.light,
+    ),
+    appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+  );
+
+  static ThemeData get dark => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.dark,
+    ),
+    appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+  );
+}
+
---
+++ lib/shared/providers/theme_provider.dart
@@ -0,0 +1,28 @@
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeProvider = NotifierProvider<ThemeNotifier, bool>(ThemeNotifier.new);
+
+class ThemeNotifier extends Notifier<bool>{
+  static const _themeKey = 'isDarkMode';
+
+  @override
+  bool build() {
+    _loadTheme();
+    return false;
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    state = prefs.getBool(_themeKey) ?? false;
+  }
+
+  Future<void> toggleTheme() async {
+    state = !state;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, state);
+  }
+
+}
+
